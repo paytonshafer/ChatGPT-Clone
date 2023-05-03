1de673a68e538213f47cc60304d6273e05e4a1ce
paytonshafer <paytonshafer@gmail.com>
2023-05-02 22:20:28 -0400

    finish base app

---
+++ server.js
@@ -0,0 +1,34 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+app.use(express.json()) //allows for frontend backend communications
+app.use(cors())
+
+const API_KEY = process.env.API_KEY
+
+app.post('/completions', async (req, res) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: req.body.message}],
+            max_tokens: 10
+        })
+    }
+
+    try{
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    }catch (err){
+        console.error(err)
+    }
+})
+
+app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT))
--- src/App.js
+++ src/App.js
@@ -1,29 +1,102 @@
+import {useEffect, useState} from 'react'
+
 const App = () => {
-  return (
-    <div className="App">
-       <section className="side-bar">
-        <button>+ New chat</button>
-        <ul className="history"></ul>
-        <nav>
-            <p>Made by Payton</p>
-        </nav>
-    </section>
+    const [value, setValue] = useState('')
+    const [message, setMessage] = useState(null)
+    const [previousChats, setPreviousChats] = useState([])
+    const [curTitle, setCurTitle] = useState(null)
 
-    <section className="main">
-        <h1>PaytonGPT</h1>
-        <ul className="feed">
+    const createNewChat = () =>{
+        setMessage(null)
+        setValue('')
+        setCurTitle(null)
+    }
 
-        </ul>
-        <div className="bottom-section">
-            <div className="input-container">
-                <input/>
-                <div id="submit">➢</div>
+    const historyClick = (title) => {
+        setCurTitle(title)
+        setMessage(null)
+        setValue('')
+    }
+    
+    const getMessages = async () => {
+        const options = {
+            method: "POST",
+            body: JSON.stringify({
+                message: value
+            }),
+            headers: {
+                'Content-Type': "application/json"
+            }
+        }
+        try{
+            const response = await fetch('http://localhost:8000/completions', options)
+            const data = await response.json()
+            setMessage(data.choices[0].message)
+        }catch (err){
+            console.error(err)
+        }
+    }
+
+    useEffect(() => {
+        console.log(curTitle, value, message)
+        if(!curTitle && value && message){
+            setCurTitle(value)
+        }
+        if(curTitle && value && message){
+            setPreviousChats(prevChats => (
+                [...prevChats,
+                    {
+                        title: curTitle,
+                        role: 'user',
+                        content: value
+                    },
+                    {
+                        title: curTitle,
+                        role: message.role,
+                        content: message.content
+                    }
+
+                ]
+            ))
+        }
+
+    }, [message, curTitle])
+
+    console.log(previousChats)
+
+    const curChat = previousChats.filter(previousChats => previousChats.title === curTitle)
+    const titles = Array.from(new Set(previousChats.map(chat => chat.title)))
+
+    return (
+        <div className="App">
+        <section className="side-bar">
+            <button onClick={createNewChat}>+ New chat</button>
+            <ul className="history">
+                {titles ? titles.map((title, index) => <li key={index} onClick={() => historyClick(title)}>{title}</li>) : null}
+            </ul>
+            <nav>
+                <p>Made by Payton</p>
+            </nav>
+        </section>
+
+        <section className="main">
+            {curTitle ? <h1>{curTitle}</h1> : <h1>PaytonGPT</h1>}
+            <ul className="feed">
+                {curChat ? curChat.map((mes, index) => <li key={index}>
+                    <p className='role'>{mes.role}</p>
+                    <p>{mes.content}</p>
+                </li>) : null}
+            </ul>
+            <div className="bottom-section">
+                <div className="input-container">
+                    <input value={value} onChange={(e) => setValue(e.target.value)}/>
+                    <div id="submit" onClick={getMessages}>➢</div>
+                </div>
+                <p className="info">ChatGPT March 23 Version</p>
             </div>
-            <p className="info">ChatGPT March 23 Version</p>
+        </section>
         </div>
-    </section>
-    </div>
-  );
+    );
 }
 
 export default App;
